58649c126b547719335a44e8f642a6cc2f540f14
BAYYYAW <59200158+BAYYYAW@users.noreply.github.com>
2020-02-26 03:36:13 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -7,8 +7,18 @@ function myFunction() {
   }
 }
 
+function myFunction1() {
+  var q = document.getElementById("navbar1");
+  if (q.style.height === "100%"){
+      q.style.height = "0%"
+  } else {
+      q.style.height = "100%"
+  }
+}
+
 var y = document.getElementById("btn");
 var z = document.getElementById("navbar");
+var w = document.getElementById("btn1");
 
 window.onscroll = function() {scrollFunction()};
 
@@ -16,12 +26,15 @@ function scrollFunction(){
   if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 90) {
     y.style.bottom = "20px";
     y.style.opacity = "1";
-
     z.style.opacity = "1";
   } else {
     y.style.bottom = "-200px";
     y.style.opacity = "0";
-
     z.style.opacity = "0";
+  }
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+    w.style.top = "670px";
+  } else {
+    w.style.top = "20px";
   }
 }
